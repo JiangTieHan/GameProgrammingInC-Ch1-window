7ac2d232704aceedd0711df2167d707f30d57eae
Xiaolei Jiang <xjiang866@gmail.com>
2022-08-31 16:27:34 -0700

    Add project files.

---
+++ Game.h
@@ -0,0 +1,13 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#pragma once
+#include "SDL/SDL.h"
+
+// Game class
+
