5c1555f7052be861c1aac49771c8fde01dd6f269
Xiaolei Jiang <xjiang866@gmail.com>
2022-08-31 16:39:06 -0700

    Created Game class skeleton

--- Game.h
+++ Game.h
@@ -10,4 +10,29 @@
 #include "SDL/SDL.h"
 
 // Game class
+class Game
+{
+public:
+	Game();
 
+	// Initialize the game
+	bool Initialize();
+
+	// Runs the game loop until the game is over
+	void RunLoop();
+
+	// Shutdown the game
+	void Shutdown();
+
+private:
+	// Helper functions for the game loop
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+
+	// Window created by SDL
+	SDL_Window* mWindow;
+
+	// Game should continue to run
+	bool mIsRunning;
+};
